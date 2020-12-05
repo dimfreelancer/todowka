69577d5dda0813c96e2aa3239f2bc137d0b63f19
dim freelancer <dimfreelancerdn@gmail.com>
2020-12-05 15:38:45 +0300

    created initTodo()

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,15 @@
 
 console.log('hello');
 
+
+
+const initTodo = (selector) => {
+    const wrapper = document.querySelector(selector);
+    wrapper.classList.add('container');
+
+    wrapper.textContent = 'Hello world';
+}
+
+
+
+initTodo('.app');
